7cb62b0f0180deacb4312200b94c36bb4aa9c847
ghostp13409 <gajjarparth09@gmail.com>
2024-08-22 20:30:26 -0400

    update

--- Ex_Files_React_js_EssT/Exercise Files/Ch04/04_03/start/react-app/src/App.js
+++ Ex_Files_React_js_EssT/Exercise Files/Ch04/04_03/start/react-app/src/App.js
@@ -1,9 +1,23 @@
 import "./App.css";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [emotion, setEmotion] = useState("Happy");
+  const [secondary, setSecondary] = useState("Tired");
+  useEffect(() => {
+    console.log(`It's ${emotion} around here!`);
+  }, [emotion]);
+  useEffect(()=>{
+    console.log(`It's ${secondary} around here!`);
+  }, [secondary])
   return (
     <div className="App">
-      <h1>Hello from React</h1>
+      <h1>Current Emotion is {emotion} </h1>
+      <button onClick={() => setEmotion("Sad")}>Sad</button>
+      <button onClick={() => setEmotion("Excited")}>Excited</button>
+      <h2>Current secondary emotion is {secondary}</h2>
+
+      <button onClick={() => setSecondary("Grateful")}>Grateful</button>
     </div>
   );
 }
