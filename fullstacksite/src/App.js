7562139f46ecd7d50bf88c2020b9ae92db00dca0
ghostp13409 <gajjarparth09@gmail.com>
2024-08-26 16:02:11 -0400

    udpate

--- fullstacksite/src/App.js
+++ fullstacksite/src/App.js
@@ -1,24 +1,24 @@
 import './App.css';
+import './index.css'
 import HomePage from './pages/HomePage';
 import AboutPage from './pages/AboutPage';
 import ArticlePage from './pages/ArticlePage';
 import ArticleListPage from './pages/ArticleListPage';
 import NotFoundPage from './pages/NotFoundPage';
+import NavBar from './NavBar';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
-      <h1>
-        My Awesome Blog
-      </h1>
+      <NavBar />
       <div id='page-body'>
         <Routes>
           <Route path='/' element={<HomePage />} />
           <Route path='/about' element={<AboutPage />} />
-          <Route path='/articlelist' element={<ArticleListPage />} />
-          <Route path='/article/:articleId' element={<ArticlePage />} />
+          <Route path='/articles' element={<ArticleListPage />} />
+          <Route path='/articles/:articleId' element={<ArticlePage />} />
           <Route path='/*' element={<NotFoundPage />} />
         </Routes>
       </div>
