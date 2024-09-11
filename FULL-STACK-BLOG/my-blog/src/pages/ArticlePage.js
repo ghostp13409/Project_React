b8467c71d7264621bd443ccb78010f1212b25713
ghostp13409 <gajjarparth09@gmail.com>
2024-09-11 14:54:08 -0400

    update

--- FULL-STACK-BLOG/my-blog/src/pages/ArticlePage.js
+++ FULL-STACK-BLOG/my-blog/src/pages/ArticlePage.js
@@ -1,10 +1,21 @@
+import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import articles from "./article-content";
 import NotFoundPage from "./NotFoundPage";
+import axios from "axios";
 
 const ArticlePage = () => {
+  const [ articleInfo, setArticleInfo] = useState({ upvotes: 0, commnets: []});
   const {articleId} = useParams();
 
+  useEffect( async ()=>{
+
+    const response = await axios.get(`http:/localhost:8000/api/articles/${articleId}`);
+    const newArticleInfo = response.data;
+
+    setArticleInfo(newArticleInfo);
+  }, []);
+  
   const article = articles.find((article) => article.name === articleId);
 
   if(!article){
@@ -12,7 +23,9 @@ const ArticlePage = () => {
   }
   return (
     <>
+      
       <h1>{article.title}</h1>
+      <p>This article has {articleInfo.upvotes} upvote(s).</p>
       {article.content.map((paragraph, i) => (
         <p key={i}>{paragraph}</p>
       ))}
