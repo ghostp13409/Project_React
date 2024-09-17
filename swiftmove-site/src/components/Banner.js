0f32b7c8d0cc2e4c7cb87e40d3b9859e67cf20d6
Parth Gajjar <gajjarparth09@gmail.com>
2024-09-17 14:52:16 -0400

    update

---
+++ swiftmove-site/src/components/Banner.js
@@ -0,0 +1,19 @@
+const Banner = () => {
+  return (
+    <>
+      <div class="banner">
+        <div className="banner_text_div">
+          <h1 className="banner_text">
+            Welcome to the World of &nbsp;
+              <img
+                className="banner_image"
+                src="./images/logo-no-background.svg"
+              />.
+          </h1>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Banner;
---
+++ swiftmove-site/src/components/Navbar.js
@@ -0,0 +1,25 @@
+
+const Navbar = () => {
+  return(
+    <>
+      <nav className="navbar">
+        <ul className="navbar-ul">
+            <li className="navbar-li">
+                <a href="#">Home</a>
+            </li>
+            <li className="navbar-li">
+                <a href="#">About</a>
+            </li>
+            <li className="navbar-li">
+                <a href="#">Login</a>
+            </li>
+            <li className="navbar-li">
+                <a href="#">Logout</a>
+            </li>
+        </ul>
+      </nav>
+    </>
+  )
+}
+
+export default Navbar;
